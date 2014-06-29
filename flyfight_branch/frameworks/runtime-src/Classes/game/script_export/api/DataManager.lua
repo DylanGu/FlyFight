de3c27c1a20ef188f663b1a384d6475515a9d0b9
SnowCold <snowcold.develop@gmail.com>
2014-06-29 23:00:08 +0800

    6_29_2

---
+++ flyfight_branch/frameworks/runtime-src/Classes/game/script_export/api/DataManager.lua
@@ -0,0 +1,31 @@
+
+--------------------------------
+-- @module DataManager
+
+--------------------------------
+-- @function [parent=#DataManager] getCurrentUserInfo 
+-- @param self
+-- @return UserInfo#UserInfo ret (return value: UserInfo)
+        
+--------------------------------
+-- @function [parent=#DataManager] changerUser 
+-- @param self
+-- @param #int int
+        
+--------------------------------
+-- @function [parent=#DataManager] getBaseData 
+-- @param self
+-- @param #int int
+-- @return BaseData#BaseData ret (return value: BaseData)
+        
+--------------------------------
+-- @function [parent=#DataManager] registerBaseData 
+-- @param self
+-- @param #BaseData basedata
+        
+--------------------------------
+-- @function [parent=#DataManager] GetInstance 
+-- @param self
+-- @return DataManager#DataManager ret (return value: DataManager)
+        
+return nil
